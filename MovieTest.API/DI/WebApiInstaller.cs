a0f43b29e9f77eb9c64b7642f6c47224840515b4
mgutierrez <manuel.gutierrez@armor.com>
2017-03-14 18:10:54 -0500

    Configure Castle Windsor in WebApi project

---
+++ MovieTest.API/DI/WebApiInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+using MovieTest.Domain.DI;
+
+namespace MovieTest.API.DI
+{
+    public class WebApiInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+            Classes
+                .FromThisAssembly()
+                .BasedOn<ApiController>()
+                .LifestyleScoped());
+
+            container.Install(new DomainInstaller());
+        }
+    }
+}
--- MovieTest.API/Global.asax.cs
+++ MovieTest.API/Global.asax.cs
@@ -1,13 +1,29 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Dispatcher;
 
+using Castle.Windsor;
+
+using MovieTest.API.DI;
 using MovieTest.Domain;
 
 namespace MovieTest.API
 {
+
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private readonly IWindsorContainer container;
+
+        // Since IWindsorContainer itself implements IDisposable, you should create and configure the container in the application's constructor 
+        // so that you can dispose it when the application exits
+        public WebApiApplication()
+        {
+            this.container = new WindsorContainer().Install(new WebApiInstaller());
+        }
+
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerActivator), new WindsorCompositionRoot(this.container));
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
             // AutoMapper Configuration
