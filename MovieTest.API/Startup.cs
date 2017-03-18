8d65e776c540731a1ec8a031365b65212bf831a5
mgutierrez <manuel.gutierrez@armor.com>
2017-03-17 19:18:16 -0500

    Add OWIN in MovieTest.API project and setup Startup class

---
+++ MovieTest.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Castle.Windsor;
+using Microsoft.Owin;
+using MovieTest.API.DI;
+using MovieTest.Domain;
+using Owin;
+
+[assembly: OwinStartup(typeof(MovieTest.API.Startup))]
+
+namespace MovieTest.API
+{
+    public class Startup
+    {
+        private readonly IWindsorContainer container;
+
+        // Since IWindsorContainer itself implements IDisposable, you should create and configure the container in the application's constructor 
+        // so that you can dispose it when the application exits
+        public Startup()
+        {
+            this.container = new WindsorContainer().Install(new WebApiInstaller());
+        }
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+
+            // Replace IHttpControllerActivator by Custom Windsor IHttpControllerActivator type so it can resolve and inject dependencies
+            httpConfiguration.Services.Replace(typeof(IHttpControllerActivator), new WindsorCompositionRoot(this.container));
+
+            WebApiConfig.Register(httpConfiguration);
+            appBuilder.UseWebApi(httpConfiguration);
+
+            MapperConfiguration.Initialize();
+        }
+    }
+}
