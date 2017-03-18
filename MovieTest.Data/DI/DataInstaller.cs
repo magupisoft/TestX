5d9852794bcc15a61ec9f453aa9149c42acc3e56
mgutierrez <manuel.gutierrez@armor.com>
2017-03-17 19:18:49 -0500

    Configure IMoviesrepository in DataInstaller for Castle Windsor Container

--- MovieTest.Data/DI/DataInstaller.cs
+++ MovieTest.Data/DI/DataInstaller.cs
@@ -2,12 +2,16 @@
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 
+using MovieTest.Data.Repositories;
+
 namespace MovieTest.Data.DI
 {
     public class DataInstaller : IWindsorInstaller
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            container.Register(
+                Component.For<IMoviesrepository>().ImplementedBy<MoviesRepository>().LifestyleTransient());
             container.Register(
                 Component.For<MoviesDbContext>().LifestyleTransient());
         }
